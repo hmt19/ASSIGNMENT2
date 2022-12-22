c229a30ab214011ba7305f79197e1ef354c5941e
hmt19 <120439254+hmt19@users.noreply.github.com>
2022-12-22 11:15:32 +0530

    Add files via upload

---
+++ Q2 A2.py
@@ -0,0 +1,7 @@
+#QUESTION 2 :
+name = "Harsh Arora"
+SID = 22105016
+department_name = "Electronics and Communication Engineering"
+CGPA= 9.9
+
+print(f"Hey,{name} Here!\nMy SID is {SID} \nI am from {department_name} department and my CGPA is {CGPA}")
